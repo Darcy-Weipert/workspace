97916dd97d9090d010812a2acad68a7aea3580cd
Darcy-Weipert <144193277+Darcy-Weipert@users.noreply.github.com>
2023-10-08 14:27:18 -0500

    updated animation on forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./styles.css";
+import ReactDOM from "react-dom";
+import ReactAnimatedWeather from "react-animated-weather";
 
 export default function Forecast() {
   return (
@@ -7,42 +9,73 @@ export default function Forecast() {
       <div className="forecastBox">
         <div className="day">
           <h4>Wed</h4>
-          <p>☀</p>
+
+          <ReactAnimatedWeather
+            icon="CLEAR_DAY"
+            color="yellow"
+            size={25}
+            animate={true}
+          />
           <p>
             72° <strong>92°</strong>
           </p>
         </div>
         <div className="day">
           <h4>Thu</h4>
-          <p>☀</p>
+          <ReactAnimatedWeather
+            icon="CLEAR_DAY"
+            color="yellow"
+            size={25}
+            animate={true}
+          />
           <p>
             71° <strong>93°</strong>
           </p>
         </div>
         <div className="day">
           <h4>Fri</h4>
-          <p>☀</p>
+          <ReactAnimatedWeather
+            icon="CLEAR_DAY"
+            color="yellow"
+            size={25}
+            animate={true}
+          />
           <p>
             73° <strong>94°</strong>
           </p>
         </div>
         <div className="day">
           <h4>Sat</h4>
-          <p>☀</p>
+          <ReactAnimatedWeather
+            icon="CLEAR_DAY"
+            color="yellow"
+            size={25}
+            animate={true}
+          />
           <p>
             71° <strong>93°</strong>
           </p>
         </div>
         <div className="day">
           <h4>Sun</h4>
-          <p>☀</p>
+          <ReactAnimatedWeather
+            icon="CLEAR_DAY"
+            color="yellow"
+            size={25}
+            animate={true}
+          />
           <p>
             70° <strong>92°</strong>
           </p>
         </div>
         <div className="day">
           <h4>Mon</h4>
-          <p>☀</p>
+          <ReactAnimatedWeather
+            icon="CLEAR_DAY"
+            color="yellow"
+            size={25}
+            animate={true}
+          />
           <p>
             70° <strong>93°</strong>
           </p>
