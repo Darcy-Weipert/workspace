f827135642b04596f4ae15d7295e47e867aa0424
Darcy-Weipert <144193277+Darcy-Weipert@users.noreply.github.com>
2023-10-08 14:29:17 -0500

    updated animation in current weather

--- src/Current.js
+++ src/Current.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./styles.css";
+import ReactDOM from "react-dom";
+import ReactAnimatedWeather from "react-animated-weather";
 
 export default function Current() {
   return (
     <div className="Current-temp">
       <div className="boxes">
         <div className="box">
-          <h2>⛅ 90°F </h2>
+          <h2>
+            <ReactAnimatedWeather
+              icon="PARTLY_CLOUDY_DAY"
+              color="black"
+              size={30}
+              animate={true}
+            />{" "}
+            90°F{" "}
+          </h2>
         </div>
         <div className="box">
           <h3>Partly Cloudy</h3>
