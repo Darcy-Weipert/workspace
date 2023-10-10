ef8dc1f30acdc8fa88d4d1705625e19471c202f7
Darcy-Weipert <144193277+Darcy-Weipert@users.noreply.github.com>
2023-10-09 21:55:38 -0500

    moved footer to proper component

--- src/App.js
+++ src/App.js
@@ -8,13 +8,3 @@ export default function App() {
     </div>
   );
 }
-<footer>
-  This project was coded by{" "}
-  <a href="https://tourmaline-swan-bb7a93.netlify.app/" target="_blank">
-    Darcy Weipert
-  </a>{" "}
-  and is{" "}
-  <a href="https://github.com/Darcy-Weipert/workspace.git" target="_blank">
-    open-sourced on Github
-  </a>
-</footer>;
--- src/index.js
+++ src/index.js
@@ -18,6 +18,19 @@ root.render(
         <Current />
         <Forecast />
       </div>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://tourmaline-swan-bb7a93.netlify.app/" target="_blank">
+          Darcy Weipert
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Darcy-Weipert/react-weather-app.git"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   </StrictMode>
 );
