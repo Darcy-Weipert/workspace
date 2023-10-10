7b0cecf292470ea81f5a8fcca886a140b1a8532c
Darcy-Weipert <144193277+Darcy-Weipert@users.noreply.github.com>
2023-10-09 21:57:31 -0500

    updated the correct netlify project

--- src/index.js
+++ src/index.js
@@ -20,12 +20,12 @@ root.render(
       </div>
       <footer>
         This project was coded by{" "}
-        <a href="https://tourmaline-swan-bb7a93.netlify.app/" target="_blank">
+        <a href="https://resilient-kitten-71b878.netlify.app/" target="_blank">
           Darcy Weipert
         </a>{" "}
         and is{" "}
         <a
-          href="https://github.com/Darcy-Weipert/react-weather-app.git"
+          href="https://github.com/Darcy-Weipert/workspace.git"
           target="_blank"
         >
           open-sourced on Github
