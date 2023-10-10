24501505399cd584ea3b231d06ef249ca5d26c1b
Darcy-Weipert <144193277+Darcy-Weipert@users.noreply.github.com>
2023-10-09 21:58:33 -0500

    moved the footer

--- src/index.js
+++ src/index.js
@@ -18,19 +18,16 @@ root.render(
         <Current />
         <Forecast />
       </div>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://resilient-kitten-71b878.netlify.app/" target="_blank">
-          Darcy Weipert
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Darcy-Weipert/workspace.git"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
     </div>
+    <footer>
+      This project was coded by{" "}
+      <a href="https://resilient-kitten-71b878.netlify.app/" target="_blank">
+        Darcy Weipert
+      </a>{" "}
+      and is{" "}
+      <a href="https://github.com/Darcy-Weipert/workspace.git" target="_blank">
+        open-sourced on Github
+      </a>
+    </footer>
   </StrictMode>
 );
